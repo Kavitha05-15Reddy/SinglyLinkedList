ff488b36c8287d783c9b8d7c86c12ca29e35d046
Kavitha05-15Reddy <pasalakavitha92@gmail.com>
2024-03-30 14:51:07 +0530

    commit

---
+++ SinglyLinkedList/Methods.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglyLinkedList
+{
+   public class Node
+   {
+        public int Data;
+        public Node Next;
+        public Node(int data)
+        {
+            Data = data;
+            Next = null;
+        }
+   }
+    public class LinkedList
+    {
+        private Node head;
+        public LinkedList()
+        {
+            head=null;
+        }
+        public void InsertB(int value)
+        {
+            Node newNode = new Node(value);
+            newNode.Next = head;
+            head = newNode;
+        }
+        public void InsertE(int value)
+        {
+            Node newNode = new Node(value);
+            if (head == null)
+            {
+                head=newNode;
+            }
+            else
+            {
+                Node current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+        public void Remove(int data)
+        {
+            if (head == null)
+            {
+                return;
+            }
+            if (head.Data == data)
+            {
+                head = head.Next;
+                return;
+            }
+            Node current = head;
+            while (current.Next != null)
+            {
+                if(current.Next.Data == data)
+                {
+                    current.Next = current.Next.Next;
+                    return;
+                }
+                current = current.Next;
+            }
+        }
+        public void Display()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                Console.WriteLine(current.Data + " ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ SinglyLinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList l = new LinkedList();    
+            l.InsertB(1);
+            l.InsertB(2);
+            l.InsertE(3);
+            l.InsertE(4);
+            l.Remove(2);
+            l.Display();
+        }
+    }
+}
